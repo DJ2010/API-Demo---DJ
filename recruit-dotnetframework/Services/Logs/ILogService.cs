a3e750b3d716e7a0e9caf48d939682e28292d60d
Deepa <deepa.johnson85@gmail.com>
2025-07-08 10:30:09 +1200

    adding ignored files

---
+++ recruit-dotnetframework/Services/Logs/ILogService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recruit_dotnetframework.Services.Logs
+{
+    public interface ILogService
+    {
+        void LogInfo(string message);
+        void LogError(string message, Exception ex = null);
+    }
+}
---
+++ recruit-dotnetframework/Services/Logs/LogService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace recruit_dotnetframework.Services.Logs
+{
+    public class LogService : ILogService
+    {
+        public void LogInfo(string message)
+        {
+            //stubbed code, can be replaced with log4net or write to file/db
+            Debug.WriteLine($"[INFO] {DateTime.UtcNow} - {message}");
+        }
+        public void LogError(string message, Exception ex = null)
+        {
+            Debug.WriteLine($"[ERROR] {DateTime.UtcNow} - {message}");
+
+            if (ex != null)
+            {
+                Debug.WriteLine($"Exception: {ex.Message}");
+                Debug.WriteLine($"StackTrace: {ex.StackTrace}");
+            }
+        }
+
+    }
+}
